2874b58c5f53ed914417c4a90857fa8acdcb187a
flitbit <phillip@flitbit.com>
2013-06-20 16:08:38 -0600

    added tests to verify digital signatures work

--- examples/initialization-example.js
+++ examples/initialization-example.js
@@ -1,11 +1,12 @@
 var claims = require('..')
+, path = require('path')
 // this sample requires that development dependencies are present
 , config = require('nconf')
 , expect = require('expect.js')
 ;
 
 // load the config from a file...
-config.file('./sample-config.json');
+config.file(path.resolve('./test/test_config.json'));
 
 expect(claims.isConfigured).to.be(false);
 
@@ -13,3 +14,11 @@ claims(config);
 
 expect(claims.isConfigured).to.be(true);
 
+var signer = claims.signer.create('global')
+, verifier = claims.verifier.create('global')
+, data = "this is some signed data"
+;
+
+var signature = signer.sign(data);
+
+expect(verifier.verify(data, signature)).to.be(true);
--- test/package_exports_tests.js
+++ test/package_exports_tests.js
@@ -2,21 +2,18 @@ var claims = require('..')
 , expect = require('expect.js')
 ;
 
-describe('claims', function() {
+describe('when the `claims` module is imported', function() {
 
-	describe('when the `claims` module is imported', function() {
+	it('it exports #signer', function() {
+		expect(claims).to.have.property('signer');
+	});
 
-		it('it exports #signer', function() {
-			expect(claims).to.have.property('signer');
-		});
+	it('it exports #verifier', function() {
+		expect(claims).to.have.property('verifier');
+	});
 
-		it('it exports #ticket', function() {
-			expect(claims).to.have.property('ticket');
-		});
+	it('it exports #claim', function() {
+		expect(claims).to.have.property('claim');
+	});
 
-		it('#isConfigured is false before being configured', function() {
-			expect(claims).to.have.property('isConfigured');
-			expect(claims.isConfigured).to.be(false);
-		});
-	})
-})
+});
