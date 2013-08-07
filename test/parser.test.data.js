fb8649ab21539680f2f0498cc59c4448da08040d
Michael Trotter <spicydonuts@gmail.com>
2013-08-07 15:12:04 -0600

    fixed test data to match updated jsonschema for claims ids

--- test/parser.test.data.js
+++ test/parser.test.data.js
@@ -6,28 +6,28 @@ module.exports = JSON.stringify({
 	"version": ticket.version,
 	"claimsets": {
 		"0": {
-			"id": 0,
+			"id": "0",
 			"claims": {
 				"1": {
-					"id": 1,
+					"id": "1",
 					"kind": "i",
 					"name": "super",
 					"value": "NS"
 				},
 				"2": {
-					"id": 2,
+					"id": "2",
 					"kind": "i",
 					"name": "commissioner",
 					"value": "XY"
 				},
 				"4": {
-					"id": 4,
+					"id": "4",
 					"kind": "i",
 					"name": "realm",
 					"value": "ns"
 				},
 				"8": {
-					"id": 8,
+					"id": "8",
 					"kind": "i",
 					"name": "tenant",
 					"value": "pws"
@@ -35,10 +35,10 @@ module.exports = JSON.stringify({
 			}
 		},
 		"14": {
-			"id": 14,
+			"id": "14",
 			"claims": {
 				"8": {
-					"id": 8,
+					"id": "8",
 					"kind": "i",
 					"value": "testValue"
 				}
